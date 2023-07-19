b325186325296a22541a86e0612964f0f308d16d
Daniele Benfica <103818625+danibenfica@users.noreply.github.com>
2023-07-19 13:37:31 -0300

    Corrigindo erro de importação do css

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-const swaggerUi = require('swagger-ui-express')
-const swaggerOptions = {customCssUrl: '/swagger-ui-css'}
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = { customCssUrl: '/swagger-ui.css' };
 const routes = require('./src/routes');
 const authDocProducao = require('./src/middlewares/authDoc');
 const app = express();
-require("dotenv").config();
+require('dotenv').config();
 
 
 
@@ -22,19 +22,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 if(process.env.NODE_ENV !== 'test'){
-    const swaggerFile = require('./swagger/swagger_output.json')
-    app.get('/', (req, res) => {/* #swagger.ignore = true */ res.redirect('/doc');});
+    const swaggerFile = require('./swagger/swagger_output.json');
+    app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
     app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 }
+
 
 
 routes(app);
 
 
 
-if(process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT} `));
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
 
 module.exports = app;
